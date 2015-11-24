9075e0751521fccff5dc98d6323c6d9a7aab9917
Brian Herbert <Brian.Herbert@webuyanycar.com>
2015-11-24 14:57:12 +0000

    Saving Data (hard coded product ID)

--- APM/APM.Client/common/productResource.js
+++ APM/APM.Client/common/productResource.js
@@ -11,6 +11,9 @@
             ]);
 
     function productResource($resource, appSettings) {
-        return $resource(appSettings.serverPath + "/api/products/:search");
+        return $resource(appSettings.serverPath + "/api/products/:id", null,
+        {
+            'update': { method: 'PUT' }
+        });
     }
 }());
